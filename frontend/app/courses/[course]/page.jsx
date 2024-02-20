d2c1b64610810082fec5b9d3606372c6d742593d
mohamednashar <105996910+mohamednashar@users.noreply.github.com>
2024-02-20 02:11:34 +0200

    z

--- frontend/app/courses/[course]/page.jsx
+++ frontend/app/courses/[course]/page.jsx
@@ -1,50 +1,95 @@
-"use client"
-import DownloadButton from '@/app/components/DownloadButton';
-import React, { useState } from 'react';
-import { IoMdMenu } from "react-icons/io";
+"use client";
+import DownloadButton from "@/app/components/DownloadButton";
+import React, { useEffect, useState } from "react";
+
 const videos = [
   {
     id: 1,
-    title: 'Lecture 1',
-    src: 'https://www.youtube.com/embed/AovUY4OqyNM',
-    slideUrl:"https://drive.google.com/file/d/1r6mVeVs4uk0u6_Z2Ls8sF2Bk-2rCAstr/view?usp=sharing"
+    title: "Lecture 1",
+    src: "https://www.youtube.com/embed/AovUY4OqyNM",
+    slideUrl:
+      "https://drive.google.com/file/d/1r6mVeVs4uk0u6_Z2Ls8sF2Bk-2rCAstr/view?usp=sharing",
   },
   {
     id: 2,
-    title: 'Lecture 2',
-    src: 'https://www.youtube.com/embed/z-4ZnPffbvc',
-    slideUrl:"https://drive.google.com/file/d/1uCNavikjGzmA9kZUUTJcQ_dTkbcVFkia/view?usp=drive_link"
+    title: "Lecture 2",
+    src: "https://www.youtube.com/embed/z-4ZnPffbvc",
+    slideUrl:
+      "https://drive.google.com/file/d/1uCNavikjGzmA9kZUUTJcQ_dTkbcVFkia/view?usp=drive_link",
   },
   // Add more videos as needed
 ];
+
 const VideoSidebar = ({ onSelectVideo, isOpen }) => {
+  const [currentVid, setCurrentVid] = useState(videos[0]);
+  const [scroll, setIsScroll] = useState(false);
+
+  const selectVidIndex = (video) => {
+    setCurrentVid(video);
+  };
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollY = window.scrollY || document.documentElement.scrollTop;
+
+      // Set a threshold of 84px
+      const threshold = 84;
+
+      // Toggle the class based on scroll position
+      setIsScroll(scrollY >= threshold);
+    };
+
+    // Add scroll event listener
+    window.addEventListener("scroll", handleScroll);
+
+    // Clean up the event listener on component unmount
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
   return (
-    <div className={`md:w-1/4  bg-white  dark:bg-gray-700 h-screen overflow-y-auto transition-all duration-700 ease-in-out ${isOpen ? 'block' : 'hidden'} `}>
-    <div className="p-4">
-      <h2 className="text-lg font-bold mb-4">Lectures</h2>
-      <ul>
-        {videos.map((video) => (
-          <li key={video.id} className="cursor-pointer hover:bg-gray-300 dark:hover:bg-gray-800 p-2 rounded-md" onClick={() => onSelectVideo(video)}>
-            {video.title}
-          </li>
-        ))}
-      </ul>
+    <div
+      className={`rounded-xl md:w-1/4 bg-white dark:bg-gray-700 h-[100vh] overflow-y-auto  ${
+        scroll ? "sticky right-5 top-0" : ""
+      }`}
+    >
+      <div className="p-4">
+        <h2 className="text-lg font-bold mb-4 border-b-2 pb-2">Lectures</h2>
+        <ul>
+          {videos.map((video) => (
+            <li
+              key={video.id}
+              className={`cursor-pointer p-2 rounded-md last:border-none transition-all duration-200 mb-2 ${
+                currentVid.id === video.id ? "bg-gray-300" : ""
+              }`}
+              onClick={() => {
+                onSelectVideo(video);
+                selectVidIndex(video);
+              }}
+            >
+              {video.title}
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
-  </div>
   );
 };
 
 const VideoPlayer = ({ videoSrc }) => {
   return (
-    <div className="md:flex-1">
-      <iframe className="w-full md:h-screen min-h-[300px]" src={videoSrc} frameBorder="0" allowFullScreen="allowFullScreen"></iframe>
+    <div className="md:flex-1 bg-gray-300 rounded-3xl p-5 min-h-[300px] h-[500px] shadow-md">
+      <iframe
+        className="w-3/4 mx-auto md:h-full min-h-[300px] rounded-2xl"
+        src={videoSrc}
+        frameBorder="0"
+        allowFullScreen="allowFullScreen"
+      ></iframe>
     </div>
   );
 };
 
-const IndexPage = ({params}) => {
-  
-  const [isOpen, setIsOpen] = useState(false);
+const IndexPage = ({ params }) => {
+  const [isOpen, setIsOpen] = useState(true);
   const [currentVideo, setCurrentVideo] = useState(videos[0]);
 
   const toggleSidebar = () => {
@@ -53,27 +98,33 @@ const IndexPage = ({params}) => {
 
   const selectVideo = (video) => {
     setCurrentVideo(video);
-    if (isOpen) {
-      setIsOpen(false); // Close the sidebar when a video is selected on small screens
-    }
   };
 
   return (
-    <>
-          <h2 className='text-center text-3xl my-3 font-semibold'>{params.course.replace(/%20/g, " ")}</h2>
-
-    <div className="md:flex">
-      <VideoPlayer videoSrc={currentVideo.src} />
-      <div >
-        <button className="p-2" onClick={toggleSidebar}>
-           <IoMdMenu className="h-6 w-6" />
-        </button>
+    <div className="md:flex pt-5 mx-5 ">
+      <div className="w-full md:w-3/4 md:mr-5">
+        <VideoPlayer videoSrc={currentVideo.src} />
+        <DownloadButton />
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sit magnam
+          consequuntur deserunt non. Laborum fugiat dolorem ex suscipit iure
+          dolor reiciendis veritatis enim, doloribus officia ad ut error
+          repellendus mollitia autem necessitatibus fuga facilis obcaecati
+          cumque nihil dolores est numquam. Suscipit excepturi reiciendis
+          eligendi harum natus necessitatibus qui cum dignissimos? Ratione
+          distinctio ipsam ad neque fuga natus placeat voluptatibus quod dolorem
+          modi odio nobis accusamus, adipisci dignissimos illum nesciunt. Magnam
+          nulla delectus sapiente ullam? Reprehenderit deserunt nam quam
+          excepturi harum, debitis possimus. Illo, veniam? Facilis nostrum nemo
+          nobis illum ullam consectetur mollitia qui officia voluptates, omnis
+          natus a illo officiis animi, eveniet voluptas quam pariatur minima.
+          Explicabo nam aliquid numquam, deleniti quas saepe cupiditate omnis,
+          laboriosam inventore vitae nesciunt cum tenetur natus in earum
+        </p>
       </div>
+
       <VideoSidebar onSelectVideo={selectVideo} isOpen={isOpen} />
     </div>
-
-    <DownloadButton fileUrl={currentVideo.slideUrl} />
-      </>
   );
 };
 
--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -1,34 +1,29 @@
-"use client"
-import { ReduxProvider } from './reduxToolkit/provider/provider'
-import { Inter } from 'next/font/google'
-import './globals.css'
-import { ThemeProvider } from 'next-themes'
-import Navbar from "./components/Navbar"
-import { usePathname } from 'next/navigation'
-const inter = Inter({ subsets: ['latin'] })
+"use client";
+import { ReduxProvider } from "./reduxToolkit/provider/provider";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { ThemeProvider } from "next-themes";
+import Navbar from "./components/Navbar";
+import { usePathname } from "next/navigation";
+const inter = Inter({ subsets: ["latin"] });
 
-
-
-
-export default function RootLayout({children}) {
-  const pathname=usePathname()
+export default function RootLayout({ children }) {
+  const pathname = usePathname();
   return (
     <html lang="en">
       <head>
         <title>Platform</title>
-        <meta name='description' content='Description' />
+        <meta name="description" content="Description" />
         <link rel="icon" href="images/icon.jpg"></link>
       </head>
-      <body className={`${inter.className} bg-gray-200 dark:bg-gray-950`}>
-        <ThemeProvider attribute='class'>
-         <ReduxProvider>
-          <Navbar/>
-          <div className={pathname !== '/profile' ? 'mx-auto max-w-7xl px-2 sm:px-6 lg:px-8' : ''}>
- {children}</div>
-                
-        </ReduxProvider>
+      <body className={`${inter.className} bg-gray-200 dark:bg-gray-950 `}>
+        <ThemeProvider attribute="class">
+          <ReduxProvider>
+            <Navbar />
+            <div>{children}</div>
+          </ReduxProvider>
         </ThemeProvider>
-        </body>
+      </body>
     </html>
-  )
+  );
 }
